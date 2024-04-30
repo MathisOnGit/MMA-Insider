d5db5011736b33c4781d98055661776a58e06d2f
MathisOnGit <165158000+MathisOnGit@users.noreply.github.com>
2024-04-30 16:56:10 +0200

    javascript

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ function getData() {
        // Traitez les données comme vous le souhaitez
        console.log('Données récupérées du fichier JSON :', data);
        /// ON ECRIT LE CODE ICI ! 
-      
+     
+// afficher titre :
+
 function afficherTitre() {
 let nomjournal = data.journal.nomJournal
 console.log(nomjournal);
@@ -20,7 +22,7 @@ titleID.insertAdjacentHTML("beforeend", nomjournal);
 }
 afficherTitre()
 
-// tableau themes nav :
+// Tableau themes nav :
 
 function afficherThemes(){
 var themes = data.journal.themes;
@@ -36,7 +38,33 @@ themesID.insertAdjacentHTML("beforeend", list);
 });
 }
 afficherThemes()
-//
+
+// Titre principal, secondaire et date Article Principal :
+ 
+function articlePrincipal() {
+var articlePrincipal = data.journal.articlePrincipal;
+console.log(articlePrincipal);
+var articlePrincipalID = document.getElementById("article-principal");
+console.log(articlePrincipalID);
+var titre = data.journal.articlePrincipal.titre;
+console.log(titre);
+var theme = data.journal.articlePrincipal.theme
+console.log(theme);
+var date = data.journal.articlePrincipal.date
+console.log(date);
+var titreprincipalID = document.getElementById("titre-principal-article");
+}
+
+ /* function afficherThemeArticle() {
+    var theme = data.journal.articlePrincipal.theme;
+    console.log(theme);
+    var themeprincipalID = document.getElementById("theme-principal-article");
+    console.log(themeprincipalID);
+    themeprincipalID.insertAdjacentHTML("beforeend", theme);
+  }
+  afficherThemeArticle();
+*/
+
 
 
        /// FIN DU CODE
