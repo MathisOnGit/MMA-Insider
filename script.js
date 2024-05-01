2c1783bdfe76817ffb40348637ef450a3a0f450b
MathisOnGit <perrotmathiss@gmail.com>
2024-05-01 16:27:50 +0200

    changements

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function getData() {
        console.log('Données récupérées du fichier JSON :', data);
        /// ON ECRIT LE CODE ICI ! 
      
-// afficher titre :
+/* afficher titre :
 
 function afficherTitre() {
 let nomjournal = data.journal.nomJournal
@@ -64,6 +64,110 @@ var titreprincipalID = document.getElementById("titre-principal-article");
   }
   afficherThemeArticle();
 */
+ function afficherTitre() {
+  let nomjournal = data.journal.nomJournal
+  console.log(nomjournal);
+  let titleID = document.getElementById("nom-journal");
+  console.log(titleID);
+  titleID.insertAdjacentHTML("beforeend", nomjournal);
+  }
+  afficherTitre()
+
+function afficherLesThemes(){
+let themes = data.journal.themes;
+ console.log(themes);
+ themes.forEach(element => {
+   let themeTitre = element.nom
+   console.log(themeTitre);
+   let liste = `<li >
+     <a href="#">${themeTitre}</a>
+     </li>`;
+   let themesId = document.getElementById("themes")
+   console.log(themesId);
+   themesId.insertAdjacentHTML("beforeend",liste )
+ })
+}
+afficherLesThemes()
+ 
+function articleprincipal(){
+let articleprincipal = data.journal.articlePrincipal
+console.log(articleprincipal);
+let articleprincipalId = document.getElementById("article-principal")
+console.log(articleprincipalId);
+let title = articleprincipal.titre;
+console.log(title);
+let date = articleprincipal.date;
+console.log(date);
+let description = articleprincipal.description;
+console.log(description);
+let themes = articleprincipal.theme
+console.log(themes);
+let image = articleprincipal.image
+console.log(image);
+let articlepricipalGlobal = `<div class="fond">
+<h1>${title}</h1>
+<p>${description}</p>
+<p>${date}</p>
+<h4>${themes}</h4>
+<img src="${image}" alt="">
+</div>`
+console.log(articlepricipalGlobal);
+articleprincipalId.insertAdjacentHTML("beforeend", articleprincipalGlobal);
+}
+articleprincipal()
+/*
+function LesArticles() {
+let articles = data.journal.articles
+console.log(articles);
+articles.forEach(element => {
+let articleId = document.getElementById("articles")
+console.log(articleId);
+let titre = element.titre
+console.log(titre);
+let date  = element.date
+console.log(date);
+let theme  = element.theme
+console.log(theme);
+let image  = element.image
+console.log(image);
+let arti = `<div>
+<img src="${image}" alt="">
+<h1>${titre}</h1>
+<h3>${theme}-${date}</h3>
+<a href="">lire article</a>
+</div>
+<div>`
+console.log(arti);
+articleId.insertAdjacentHTML("beforeend",arti)
+});
+}
+function touslesauteurs() {
+let auteurs = data.journal.auteurs
+console.log(auteurs);
+auteurs.forEach(element => {
+let auteurId = document.getElementById("auteurs")
+console.log(auteurId);
+let prenom = element.prenom
+console.log(prenom);
+let expreience = element.typeExperience
+console.log(expreience);
+let presentation = element.presentation
+console.log(presentation);
+let lesauteurs = ` <div>
+<img src="" alt="">
+<h2>${prenom}</h2>
+<h3>${expreience}</h3>
+<p>${presentation}</p>
+</div>`
+console.log(lesauteurs);
+auteurId.insertAdjacentHTML("beforeend", lesauteurs)
+});
+}
+*/
+
+
+   LesArticles()
+   touslesauteurs()
 
 
 
